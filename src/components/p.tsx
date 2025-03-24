d4ff77e17fdc58be427745057bd0066a38c2628e
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-03-24 11:02:38 -0300

    feat: pass color to p component

--- src/components/p.tsx
+++ src/components/p.tsx
@@ -1,8 +1,12 @@
 import { Text, type TextProps } from "react-native";
 
+import { useColorScheme } from "../hooks/use-color-scheme";
+
 export function P({ children, style, ...props }: TextProps) {
+	const colorScheme = useColorScheme();
+
 	return (
-		<Text style={style} {...props}>
+		<Text style={[style, { color: colorScheme.foreground }]} {...props}>
 			{children}
 		</Text>
 	);
