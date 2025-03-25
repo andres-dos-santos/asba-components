febcbaf89d9d0a0514f3cfcfeac76d83804cbda3
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-03-24 21:21:45 -0300

    feat: adding input component

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,9 +2,13 @@ export const colors = {
 	dark: {
 		background: "#1C1C1C",
 		foreground: "#FAFAFA",
+		border: "#27272A",
+		borderFocused: "#52525B",
 	},
 	light: {
 		background: "#FAFAFA",
 		foreground: "#1C1C1C",
+		border: "#E4E4E7",
+		borderFocused: "#D4D4D8",
 	},
 } as const;
