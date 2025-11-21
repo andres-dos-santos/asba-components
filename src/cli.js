a7f40581a12c47b635345ef9a5b1ddae68d688a9
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-11-20 21:47:07 -0300

    feat(cli.js): create a cli for theme

---
+++ src/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs'
+import path from 'node:path'
+
+const filePath = path.join(process.cwd(), 'constants/theme.ts')
+
+const content = `
+dark: {
+  colors: {
+    primary: '#8257e6',
+    background: '#1c1c1c',
+    text: '#ffffff'
+  }
+}
+light: {
+  colors: {
+    primary: '#8257e6',
+    background: '#ffffff',
+    text: '#1c1c1c'
+  }
+}
+`;
+
+fs.mkdirSync(path.dirname(filePath), { recursive: true })
+fs.writeFileSync(filePath, content)
+
+console.log('🎨 Tema criado em: ', filePath);
