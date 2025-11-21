f71f47ccc3028934b968647de8a7ad1ca7f5c143
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-11-20 21:57:25 -0300

    feat(cli.js): update cli content

--- cli.js
+++ cli.js
@@ -6,18 +6,20 @@ import path from 'node:path'
 const filePath = path.join(process.cwd(), 'constants/theme.ts')
 
 const content = `
-dark: {
-  colors: {
-    primary: '#8257e6',
-    background: '#1c1c1c',
-    text: '#ffffff'
+export const theme = {
+  dark: {
+    colors: {
+      primary: '#8257e6',
+      background: '#1c1c1c',
+      text: '#ffffff'
+    }
   }
-}
-light: {
-  colors: {
-    primary: '#8257e6',
-    background: '#ffffff',
-    text: '#1c1c1c'
+  light: {
+    colors: {
+      primary: '#8257e6',
+      background: '#ffffff',
+      text: '#1c1c1c'
+    }
   }
 }
 `;
