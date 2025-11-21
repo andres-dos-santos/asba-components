65698d0d732e04188fb1d99083a96f9828271fb7
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-11-20 22:03:01 -0300

    feat(cli.js): update template

--- cli.js
+++ cli.js
@@ -5,15 +5,14 @@ import path from 'node:path'
 
 const filePath = path.join(process.cwd(), 'constants/theme.ts')
 
-const content = `
-export const theme = {
+const content = `export const theme = {
   dark: {
     colors: {
       primary: '#8257e6',
       background: '#1c1c1c',
       text: '#ffffff'
     }
-  }
+  },
   light: {
     colors: {
       primary: '#8257e6',
